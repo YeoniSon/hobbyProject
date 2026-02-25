ab255e35d9d8280821d129bec275dda8609d8048
YeoniSon <sjy8211@gmail.com>
2026-02-25 17:41:30 +0900

    feat: category 조회 구현
    category의 조회 부분을 구현함

--- board/src/main/java/com/example/repository/CategoryRepository.java
+++ board/src/main/java/com/example/repository/CategoryRepository.java
@@ -3,8 +3,15 @@ package com.example.repository;
 import com.example.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
+
+    List<Category> findAll();
+    List<Category> findAllByShowFalse();
+    List<Category> findAllByShowTrue();
+
+    boolean existsByName(String name);
 }
--- board/src/main/java/com/example/service/CategoryService.java
+++ board/src/main/java/com/example/service/CategoryService.java
@@ -4,9 +4,13 @@ import com.example.common.exception.BusinessException;
 import com.example.common.exception.ErrorCode;
 import com.example.domain.Category;
 import com.example.dto.request.CategoryRegisterRequest;
+import com.example.dto.response.CategoryResponse;
 import com.example.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +18,18 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+    /*
+    카테고리 관련 내용
+    - 카테고리 등록
+    - 카테고리 조회 (전체, 숨겨진 것들, 보여지는 것들)
+    - 카테고리 삭제
+    - 카테고리 수정
+     */
+
+    // 카테고리 등록
+    @Transactional
     public void register(CategoryRegisterRequest request) {
-        if (categoryRepository.findByName(request.getName()).isPresent()) {
+        if (categoryRepository.existsByName(request.getName())) {
             throw new BusinessException(ErrorCode.DUPLICATE_CATEGORY);
         }
 
@@ -29,6 +43,34 @@ public class CategoryService {
                 .name(request.getName())
                 .build();
     }
+
+    // 카테고리 조회 (전체)
+    @Transactional
+    public List<CategoryResponse> getAllCategory() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(CategoryResponse::from)
+                .toList();
+    }
+
+    // 카테고리 조회(show true)
+    @Transactional
+    public List<CategoryResponse> getShowTrueCategory() {
+        return categoryRepository.findAllByShowTrue()
+                .stream()
+                .map(CategoryResponse::from)
+                .toList();
+    }
+
+    // 카테고리 조회(show false)
+    @Transactional
+    public List<CategoryResponse> getShowFalseCategory() {
+        return categoryRepository.findAllByShowFalse()
+                .stream()
+                .map(CategoryResponse::from)
+                .toList();
+    }
+
 
 
 }
