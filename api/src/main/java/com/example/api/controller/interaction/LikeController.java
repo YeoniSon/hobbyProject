e0f628af52f63c05ab90c7b7e22a23107e4dba6b
YeoniSon <sjy8211@gmail.com>
2026-02-27 00:50:14 +0900

    feat: 댓글 좋아요 / 취소 구현
    댓글의 좋아요와 취소를 구현하는 Controller 구현

--- api/src/main/java/com/example/api/controller/interaction/LikeController.java
+++ api/src/main/java/com/example/api/controller/interaction/LikeController.java
@@ -1,6 +1,7 @@
 package com.example.api.controller.interaction;
 
 import com.example.api.security.CustomUserDetails;
+import com.example.common.enums.TargetType;
 import com.example.interaction.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,22 +21,47 @@ public class LikeController {
     게시글, 댓글별 개수
      */
     // 게시글 좋아요
-    @PostMapping("/{postId}")
+    @PostMapping("/post/{postId}")
     public ResponseEntity<String> likePost(
             @AuthenticationPrincipal CustomUserDetails userDetails,
-            @PathVariable Long postId
+            @PathVariable Long postId,
+            @RequestParam TargetType targetType
     ) {
-        likeService.postLike(userDetails.getId(), postId);
+        likeService.postLike(userDetails.getId(), postId, targetType);
         return ResponseEntity.ok("좋아요 success");
     }
 
     // 게시글 좋아요 취소
-    @PutMapping("/unLike/{postId}")
+    @PutMapping("/post/{postId}/un-like")
     public ResponseEntity<String> unLikePost(
             @AuthenticationPrincipal CustomUserDetails userDetails,
-            @PathVariable Long postId
+            @PathVariable Long postId,
+            @RequestParam TargetType targetType
     ) {
-        likeService.postUnLike(userDetails.getId(), postId);
+        likeService.postUnLike(userDetails.getId(), postId, targetType);
         return ResponseEntity.ok("좋아요 취소 success");
     }
+
+    // 댓글 좋아요
+    @PostMapping("/comment/{commentId}")
+    public ResponseEntity<String> likeComment(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable Long commentId,
+            @RequestParam TargetType targetType
+    ){
+        likeService.postLike(userDetails.getId(), commentId, targetType);
+        return ResponseEntity.ok("comment like success");
+    }
+
+    // 댓글 좋아요 취소
+    @PutMapping("/comment/{commentId}/un-like")
+    public ResponseEntity<String> unLikeComment(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable Long commentId,
+            @RequestParam TargetType targetType
+    ) {
+        likeService.postUnLike(userDetails.getId(), commentId, targetType);
+        return ResponseEntity.ok("comment unlike success");
+    }
+
 }
