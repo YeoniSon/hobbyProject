f67110c42323992bea3d09399d2408501b22d268
YeoniSon <sjy8211@gmail.com>
2026-03-01 22:08:31 +0900

    feat: Response, Request 구현

---
+++ chat/src/main/java/com/example/chat/dto/request/ChatMessageSendRequest.java
@@ -0,0 +1,12 @@
+package com.example.chat.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessageSendRequest {
+    private String content;
+}
